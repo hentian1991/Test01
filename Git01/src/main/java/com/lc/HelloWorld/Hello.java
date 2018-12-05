3ece464245ea2194d22703e857626438554d0f29
hentian1997 <45616609+hentian1997@users.noreply.github.com>
2018-12-05 22:41:54 +0800

    Update Hello.java

--- Git01/src/main/java/com/lc/HelloWorld/Hello.java
+++ Git01/src/main/java/com/lc/HelloWorld/Hello.java
@@ -2,7 +2,7 @@ package com.lc.HelloWorld;
 
 public class Hello {
     public static void main(String[] args) {
-
+            "5555555555"
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
